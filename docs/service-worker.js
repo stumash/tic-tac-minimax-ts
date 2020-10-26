bec843b06c160db5c8b515ab9fe795bb07b87799
Stuart Mashaal <smashaal@hopper.com>
2020-10-26 18:28:35 -0400

    update homepage again... 4th time

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tic-tac-minimax/docs/precache-manifest.d5cf9b39639354f693d92a23dfbb22fc.js"
+  "/tic-tac-minimax/precache-manifest.6e9fbd170d1a1d8d782acbf7ea1178fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-minimax/docs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tic-tac-minimax/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
